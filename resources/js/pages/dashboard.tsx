c2105991e6802d803e32553fa9c20220ad24b6ca
NxtPerfect <alakaxan@protonmail.com>
2025-07-21 10:52:28 +0200

    feat: changed user's stats to render as a table

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -10,10 +10,27 @@ const breadcrumbs: BreadcrumbItem[] = [
 ];
 
 export default function Dashboard({ games, username }: any) {
-    console.log(username, games);
+    const averagePlaytime = (games.map((g) => g.playtime).reduce((ac, cur) => (ac += cur), 0) / games.length).toFixed(2);
+    const playedGames = games.filter((g) => g.status !== 'planning' && g.status != 'not planning').length;
+
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Dashboard" />
+            <table>
+                <caption>Statistics</caption>
+                <tr>
+                    <th>Name</th>
+                    <th>Amount</th>
+                </tr>
+                <tr>
+                    <td>Games played</td>
+                    <td>{playedGames}</td>
+                </tr>
+                <tr>
+                    <td>Average playtime</td>
+                    <td>{averagePlaytime}h</td>
+                </tr>
+            </table>
             <ol className="list-inside list-decimal">
                 {games &&
                     games.map((game) => {
